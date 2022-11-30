660ba5647efb39002e599f63d5ea10e542f29d9c
laurahennig <laura.n.hennig@fau.de>
2022-11-30 21:59:59 +0100

    Adapt setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='DSSS_Homework5',
+      version='0.1',
+      author='Laura Hennig',
+      author_email='laura.n.hennig@fau.de',
+      packages=find_packages(),
+      install_requires=['numpy'])
+
